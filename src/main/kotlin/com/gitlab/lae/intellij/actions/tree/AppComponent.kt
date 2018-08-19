8a4accec207a2648cf535a96e2f90df034ee6861
Lae <laec@protonmail.ch>
2018-08-19 15:17:58 +1200

    Error notification.

--- src/main/kotlin/com/gitlab/lae/intellij/actions/tree/AppComponent.kt
+++ src/main/kotlin/com/gitlab/lae/intellij/actions/tree/AppComponent.kt
@@ -1,11 +1,15 @@
 package com.gitlab.lae.intellij.actions.tree
 
 import com.intellij.ide.util.PropertiesComponent
+import com.intellij.notification.Notification
+import com.intellij.notification.NotificationType
+import com.intellij.notification.Notifications
 import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.actionSystem.KeyboardShortcut
 import com.intellij.openapi.components.ApplicationComponent
 import com.intellij.openapi.keymap.Keymap
 import com.intellij.openapi.keymap.KeymapManager
+import org.apache.commons.lang3.exception.ExceptionUtils
 import java.lang.System.identityHashCode
 import java.nio.file.Paths
 
@@ -34,7 +38,17 @@ class AppComponent : ApplicationComponent {
 private fun loadActions(): List<GenAction> {
     val props = PropertiesComponent.getInstance()
     val conf = props.getValue(confKey)?.trim() ?: return emptyList()
-    return parseJsonActions(Paths.get(conf)).map(::GenAction)
+    return try {
+        parseJsonActions(Paths.get(conf)).map(::GenAction)
+    } catch (e: Exception) {
+        Notifications.Bus.notify(Notification(
+                "ActionsTree",
+                "Failed to load keymap",
+                "Failed to load keymap: $conf\n${ExceptionUtils.getStackTrace(e)}",
+                NotificationType.ERROR
+        ))
+        emptyList()
+    }
 }
 
 private fun Keymap.registerShortcuts(actions: List<GenAction>) {
