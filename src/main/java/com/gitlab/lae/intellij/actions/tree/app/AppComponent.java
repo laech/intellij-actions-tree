03055940b1298225df50d88337f799e0e3b6e5e4
Lae <llaechen@gmail.com>
2019-12-07 11:29:41 +1300

    Fix error string.

--- src/main/java/com/gitlab/lae/intellij/actions/tree/app/AppComponent.java
+++ src/main/java/com/gitlab/lae/intellij/actions/tree/app/AppComponent.java
@@ -21,6 +21,7 @@ import java.util.List;
 
 import static com.gitlab.lae.intellij.actions.tree.json.ActionNodeParser.parseJsonActions;
 import static java.util.Collections.emptyList;
+import static org.apache.commons.lang3.exception.ExceptionUtils.getStackTrace;
 
 public final class AppComponent implements ApplicationComponent {
 
@@ -89,7 +90,7 @@ public final class AppComponent implements ApplicationComponent {
             Notifications.Bus.notify(new Notification(
                     "ActionsTree",
                     "Failed to load keymap",
-                    "Failed to load keymap: $conf\n${getStackTrace(e)}",
+                    "Failed to load keymap: " + conf + "\n" + getStackTrace(e),
                     NotificationType.ERROR
             ));
             return emptyList();
